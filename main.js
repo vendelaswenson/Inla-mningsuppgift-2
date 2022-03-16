c1738b8e4a9f6f1079e8a048e11c8295e625edad
VendelaAJ <vendela.swenson@hotmail.se>
2022-03-16 11:43:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,249 @@
+class Character {
+  constructor(name, gender, height, mass, hairColor, pictureUrl, id) {
+    this.name = name;
+    this.gender = gender;
+    this.height = height;
+    this.mass = mass;
+    this.hairColor = hairColor;
+    this.pictureUrl = pictureUrl;
+    this.id = id;
+  }
+
+  compLength(data) {
+    let heightComp = Math.floor(this.height) - Math.floor(data.height);
+    if (Number(this.height) > Number(data.height)) {
+      if (this == infoOne) {
+        info1.innerHTML = `<h3>My height is ${this.height} cm and ${data.name}'s height is ${data.height} cm! That's ${heightComp} cm less than me!</h3>`;
+      } else if (this == infoTwo) {
+        info2.innerHTML = `<h3>My height is ${this.height} cm and ${data.name}'s height is ${data.height} cm! That's ${heightComp} cm less than me!</h3>`;
+      }
+    } else if (Number(this.height) < Number(data.height)) {
+      if (this == infoOne) {
+        info1.innerHTML = `<h3>My height is ${this.height} cm and ${data.name}'s height is ${data.height}cm! That's ${heightComp} cm less than me!</h3>`;
+      } else {
+        info2.innerHTML = `<h3>My height is ${this.height} cm and ${data.name}'s height is ${data.height}cm! That's ${heightComp} cm less than me!</h3>`;
+      }
+    } else {
+      if (this == infoOne) {
+        info1.innerHTML = `<h3>We both have the same height of ${this.height} cm!</h3>`;
+        info2.innerHTML = `<h3>We both have the same height of ${this.height} cm!</h3>`;
+      }
+    }
+  }
+
+  compWeight(data) {
+    const weightComp = Math.floor(this.mass) - Math.floor(data.mass);
+    const weightComp2 = Math.floor(data.mass) - Math.floor(this.mass);
+    if (Number(this.mass) > Number(data.mass)) {
+      if (this == infoOne) {
+        info1.innerHTML = `<h3>My weight is ${this.mass}kg and ${data.name} 's weight is ${data.mass} kg. That's ${weightComp} kg less than me!</h3>`;
+      } else if (this == infoTwo) {
+        info2.innerHTML = `<h3> My weight is ${this.mass} kg and ${data.name} 's weight is ${data.mass} kg. That's ${weightComp} kg less than me!</h3>`;
+      }
+    } else if (Number(this.mass) < Number(data.mass)) {
+      if (this == infoOne) {
+        info1.innerHTML = `<h3>My weight is ${this.mass} kg and ${data.name} 's weight is ${data.mass} kg. That's ${weightComp2} more than me :(</h3>`;
+      } else {
+        info2.innerHTML = `<h3>My weight is ${this.mass}kg and ${data.name} 's  weight is ${data.mass} kg. That's ${weightComp2} more than me :(</h3>`;
+      }
+    } else {
+      if (this == infoOne) {
+        info1.innerHTML = `<h3>We have the same weight of ${this.mass}kg!</h3>`;
+        info2.innerHTML = `<h3>We have the same weight of ${this.mass}kg!</h3>`;
+      }
+    }
+  }
+
+  compGender(data) {
+    if (this == infoOne) {
+      if (
+        (this.gender === "none" && data.gender === "none") ||
+        (this.gender === "n/a" && data.gender === "n/a") ||
+        (this.gender === "n/a" && data.gender === "none") ||
+        (this.gender === "none" && data.gender === "n/a")
+      ) {
+        info1.innerHTML = `<h3>Niether of us know what our gender is :((</h3>`;
+        info2.innerHTML = `<h3>Niether of us know what our gender is :((</h3>`;
+      } else if (this.gender === "n/a" || this.gender === "none") {
+        info1.innerHTML = `<h3>I don't know what my gender is :( ${data.name}'s gender is ${data.gender}</h3>`;
+      } else if (this.gender == data.gender) {
+        info1.innerHTML = `<h3>We both have the same gender of ${this.gender}</h3>`;
+      } else {
+        info1.innerHTML = `<h3>My gender is ${this.gender} and ${data.name}'s gender is ${data.gender}!</h3>`;
+      }
+    } else {
+      if (this.gender == data.gender) {
+        info2.innerHTML = `<h3>We both have the same gender of ${this.gender}</h3>`;
+      } else if (this.gender === "n/a" || this.gender === "none") {
+        info2.innerHTML = `<h3>I don't know what my gender is :( ${data.name}'s gender is ${data.gender}</h3>`;
+      } else {
+        info2.innerHTML = `<h3>My gender is ${this.gender} and ${data.name}'s gender is ${data.gender}!</h3>`;
+      }
+    }
+  }
+
+  compHaircolor(data) {
+    if (this == infoOne) {
+      if (
+        (this.hairColor === "none" && data.hairColor === "none") ||
+        (this.hairColor === "n/a" && data.hairColor === "n/a") ||
+        (this.hairColor === "n/a" && data.hairColor === "none") ||
+        (this.hairColor === "none" && data.hairColor === "n/a")
+      ) {
+        info1.innerHTML = `<h3>Niether of us have hair :((</h3>`;
+        info2.innerHTML = `<h3>Niether of us have hair :((</h3>`;
+      } else if (this.hairColor === "none" || this.hairColor === "n/a") {
+        info1.innerHTML = `<h3>I don't have any hair :( ${data.name} has really beautiful ${data.hairColor} hair</h3>`;
+      } else if (this.hairColor == data.hairColor) {
+        info1.innerHTML = `<h3>We both have the same haircolor!</h3>`;
+      } else {
+        info1.innerHTML = `<h3>My hair color is ${this.hairColor} and ${data.name} has ${data.hairColor} hair!</h3>`;
+      }
+    } else {
+      if (
+        (this.hairColor === "none" && data.hairColor === "none") ||
+        (this.hairColor === "n/a" && data.hairColor === "n/a") ||
+        (this.hairColor === "n/a" && data.hairColor === "none") ||
+        (this.hairColor === "none" && data.hairColor === "n/a")
+      ) {
+        info1.innerHTML = `<h3>Niether of us have hair :((</h3>`;
+        info2.innerHTML = `<h3>Niether of us have hair :((</h3>`;
+      } else if (this.hairColor === "none" || this.hairColor === "n/a") {
+        info2.innerHTML = `<h3>I dont have any hair :( ${data.name} has really beautiful ${data.hairColor} hair</h3>`;
+      } else if (this.hairColor == data.hairColor) {
+        info2.innerHTML = `<h3>We both have the same haircolor!</h3>`;
+      } else {
+        info2.innerHTML = `<h3>My hair color is ${this.hairColor} and ${data.name} has ${data.hairColor} hair!</h3>`;
+      }
+    }
+  }
+}
+
+const dataOne = document.querySelector(".data1");
+const dataTwo = document.querySelector(".data2");
+const showBtn = document.querySelector(".showBtn");
+const info1 = document.querySelector(".info1");
+const info2 = document.querySelector(".info2");
+const infoWrapper = document.querySelector(".infoWrapper");
+
+const getPictureUrl = (id) => {
+  switch (id) {
+    case "1":
+      return "./Bilder/LukeSkywalker.png";
+    case "2":
+      return "./Bilder/C-3PO.png";
+    case "3":
+      return "https://static.partyking.org/fit-in/1300x0/products/original/star-wars-r2-d2-kartongfigur-73775-1.jpg";
+    case "4":
+      return "https://www.scifishop.se/wp-content/uploads/2013/12/darth-vader-konst.jpg";
+    case "5":
+      return "http://sartorialgeek.com/wp-content/uploads/2018/09/960-e1538273942244.jpg";
+    case "6":
+      return "https://static.wikia.nocookie.net/swfanon/images/9/94/OwenLars.jpg/revision/latest?cb=20080202051733";
+    case "7":
+      return "http://pm1.narvii.com/6168/ff4e27ef435e7191ca5429126c31754115a93776_00.jpg";
+    case "8":
+      return "https://www.scifishop.se/wp-content/uploads/2016/07/R5-D4-Sideshow-actionfigur.jpg";
+    case "9":
+      return "https://static.wikia.nocookie.net/swfanon/images/5/57/BiggsDarklighter.jpg/revision/latest?cb=20120324222406";
+    case "10":
+      return "https://images.aftonbladet-cdn.se/v2/images/c9329b68-df83-4d47-abf0-23a6ca6784a1?fit=crop&format=auto&h=1094&q=50&w=1900&s=cb49325021ddbefe7caf4612ae07d2086ac29ccf";
+    default:
+  }
+};
+
+let btnComp = (data, compData, elem, btn) => {
+  if (data.id == elem) {
+    if (btn.className == "weight") {
+      data.compWeight(compData);
+    } else if (btn.className == "length") {
+      data.compLength(compData);
+    } else if (btn.className == "hairColor") {
+      data.compHaircolor(compData);
+    } else if (btn.className == "gender") {
+      data.compGender(compData);
+    }
+  }
+};
+
+const btn = (data) => {
+  const compareBtn = document.querySelectorAll("#compareBtn");
+  compareBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      infoWrapper.classList.remove("hidden");
+      let elem = btn.getAttribute("data-id");
+      if (data === infoOne) {
+        btnComp(infoOne, infoTwo, elem, btn);
+      } else {
+        btnComp(infoTwo, infoOne, elem, btn);
+      }
+    });
+  });
+};
+
+let infoOne;
+let infoTwo;
+let inputValue;
+let inputValue2;
+
+showBtn.addEventListener("click", () => {
+  const e = document.querySelector("#char-1");
+  const elem = document.querySelector("#char-2");
+  inputValue = e.value;
+  inputValue2 = elem.value;
+
+  if (inputValue == "null" || inputValue2 == "null") {
+    alert("You need to pick two characters!");
+  } else {
+    info1.innerHTML = "";
+    info2.innerHTML = "";
+
+    getInfo(inputValue).then((data) => {
+      infoOne = data;
+      console.log(infoOne);
+      dataOne.innerHTML = `
+      <div class="char">
+      <img src="${data.pictureUrl}" alt="${data.name}">
+      <h2 class="name">${data.name}</h2>
+      <div class="buttonContainer">
+      <button id="compareBtn" class="weight" data-id="${data.id}">Weight</button>
+      <button id="compareBtn" class="length" data-id="${data.id}">Length</button>
+      <button id="compareBtn" class="hairColor" data-id="${data.id}">Hair Color</button>
+      <button id="compareBtn" class="gender" data-id="${data.id}">Gender</button>
+  </div>
+      </div>`;
+      btn(infoOne);
+    });
+
+    getInfo(inputValue2).then((data2) => {
+      infoTwo = data2;
+      console.log(infoTwo);
+      dataTwo.innerHTML = `
+      <div class="char">
+      <img src="${data2.pictureUrl}" alt="${data2.name}">
+      <h2 class="name">${data2.name}</h2>
+      <div class="buttonContainer">
+      <button id="compareBtn" class="weight" data-id="${data2.id}">Weight</button>
+      <button id="compareBtn" class="length" data-id="${data2.id}">Length</button>
+      <button id="compareBtn" class="hairColor" data-id="${data2.id}">Hair Color</button>
+      <button id="compareBtn" class="gender" data-id="${data2.id}">Gender</button>
+      </div>
+      </div>`;
+      btn(infoTwo);
+    });
+  }
+});
+
+const getInfo = async (id) => {
+  const response = await fetch(`https://swapi.dev/api/people/${id}`);
+  const data = await response.json();
+  return new Character(
+    data.name,
+    data.gender,
+    data.height,
+    data.mass,
+    data.hair_color,
+    getPictureUrl(id),
+    id
+  );
+};
